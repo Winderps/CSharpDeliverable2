60938891b102609382453dc84fe0cca376f02b34
Winderps <homerun32295@gmail.com>
2020-02-27 00:59:23 -0500

    Finished CSharp Deliverable 2

---
+++ CSharpDeliverable2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpDeliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "";
+            string input = "";
+            int checksum = 0;
+
+            Console.Write("What is your message? ");
+            input = Console.ReadLine().ToUpper();
+
+            foreach (char c in input)
+            {
+                message += (c - 64).ToString() + "-";
+                checksum += c;
+            }
+            message = message.Remove(message.Length - 1);
+
+            Console.WriteLine($"New encoded message: {message}.");
+            Console.WriteLine($"Message checksum: {checksum}.");
+        }
+    }
+}
